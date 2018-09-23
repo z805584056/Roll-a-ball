c8f2b55d4bd476469e3ea4aecbc4fcb6cafeb408
z805584056 <z568559637@gmail.com>
2018-09-23 15:41:34 -0400

    Challenge

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,14 +8,19 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
+    public Text redCountText;
+    public Text totalCountText;
 
 
     private Rigidbody rb;
     private int count;
+    private int RedCount;
+    private int TotalCount;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
+        RedCount = 0;
         SetCountText();
         winText.text = "";
     }
@@ -27,25 +32,45 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement*speed);
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
             SetCountText();
         }
-    }
-    void SetCountText()
-    {
-        countText.text = "Count: " + count.ToString();
-        if(count >= 12)
+        else if (other.gameObject.CompareTag("Red Pick Up"))
         {
-            winText.text = "You Win!";
+            other.gameObject.SetActive(false);
+            RedCount = RedCount + 1;
+            
+            SetCountText();
         }
     }
 
-    private void Update()
+
+
+    void SetCountText()
+    {
+        countText.text = "Yellow Cube: " + count.ToString();
+        redCountText.text = "Red Cube: " + RedCount.ToString();
+        TotalCount = count - RedCount;
+        totalCountText.text = "Your Total Score: " + TotalCount.ToString();
+        
+        if ( count >= 12)
+            {
+                winText.text = "You Finished with a score of: " + TotalCount.ToString();
+            }
+
+    }
+
+
+
+
+        private void Update()
     {
         if (Input.GetKey("escape"))
             Application.Quit();
